c210c8b622288177b90735bf580e2fba99f5825c
Alexey Rusov <rolic402@mail.ru>
2020-11-25 15:14:24 +0300

    deep refactoring

--- t1_test.go
+++ t1_test.go
@@ -1,13 +1,217 @@
 package workers
 
 import (
+	"sync/atomic"
 	"testing"
+	"time"
+
+	"github.com/alrusov/misc"
 )
 
 //----------------------------------------------------------------------------------------------------------------------------//
 
-func Test1(t *testing.T) {
-	// TODO
+func TestComplexZero(t *testing.T) {
+	testComplex(t, 0)
+}
+
+func TestComplexOne(t *testing.T) {
+	testComplex(t, 1)
+}
+
+func TestComplexSmall(t *testing.T) {
+	testComplex(t, 10)
+}
+
+func TestComplexBig(t *testing.T) {
+	testComplex(t, 1000000)
+}
+
+func testComplex(t *testing.T, n int) {
+	w, err := New(nil)
+	if err == nil {
+		t.Fatalf("New(nil): Expected error but not found")
+	}
+
+	if w != nil {
+		t.Fatalf("New(nil): not null worker returned")
+	}
+
+	//-------------------------------//
+
+	check := func(fn string, maxWorker int, multithreadedThreshold int) {
+		tw := &testWorker{
+			data:      make([]int, n),
+			clean:     false,
+			withDelay: false,
+		}
+		for i := 0; i < n; i++ {
+			tw.data[i] = i
+		}
+
+		w, err := New(
+			tw,
+			MaxWorker(maxWorker),
+			MultithreadedThreshold(multithreadedThreshold),
+		)
+		if err != nil {
+			t.Fatalf("New(...): %s", err.Error())
+		}
+
+		err = w.Do()
+		if err != nil {
+			t.Fatalf("%s: %s", fn, err.Error())
+		}
+
+		for i := 0; i < n; i++ {
+			if tw.data[i] != -i {
+				t.Fatalf("%s: found %d, expected %d", fn, tw.data[i], -i)
+			}
+		}
+
+		st := int32(0)
+		threads := maxWorker
+
+		if maxWorker == 1 || multithreadedThreshold > n {
+			st = 1
+			threads = 1
+		}
+
+		if n == 0 {
+			st = 0
+		} else if n == 1 {
+			st = 1
+		}
+
+		if threads > n {
+			threads = n
+		}
+
+		if tw.singleThread != st {
+			t.Fatalf("%s: single threads count is %d, expected %d", fn, tw.singleThread, st)
+		}
+
+		if tw.started != int32(threads) {
+			t.Fatalf("%s: started %d threads, expected %d", fn, tw.started, threads)
+		}
+
+		if tw.finished != int32(threads) {
+			t.Fatalf("%s: finished %d threads, expected %d", fn, tw.finished, threads)
+		}
+	}
+
+	//-------------------------------//
+
+	check("Singlethreaded 1", 1, 0)
+	check("Singlethreaded 2", 1, n)
+	check("Singlethreaded 3", 1, n+1)
+
+	check("Multithreaded 1.1", 5, 0)
+	check("Multithreaded 1.2", 5, n)
+	check("Multithreaded 1.3", 5, n+1)
+
+	check("Multithreaded 2.1", 100, 0)
+	check("Multithreaded 2.2", 100, n)
+	check("Multithreaded 2.3", 100, n+1)
+
+	if n < 500 {
+		check("Multithreaded 3.1", n, 0)
+		check("Multithreaded 3.2", n, n)
+		check("Multithreaded 3.3", n, n+1)
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+func BenchmarkComplexSingleClean(b *testing.B) {
+	benchmarkComplex(b, b.N, true, true)
+}
+
+func BenchmarkComplexMultiClean(b *testing.B) {
+	benchmarkComplex(b, b.N, true, false)
+}
+
+func BenchmarkComplexSingle(b *testing.B) {
+	benchmarkComplex(b, b.N, false, true)
+}
+
+func BenchmarkComplexMulti(b *testing.B) {
+	benchmarkComplex(b, b.N, false, false)
+}
+
+func benchmarkComplex(b *testing.B, n int, clean bool, single bool) {
+	tw := &testWorker{
+		data:      make([]int, n),
+		clean:     clean,
+		withDelay: true,
+	}
+	for i := 0; i < n; i++ {
+		tw.data[i] = i
+	}
+
+	multithreadedThreshold := 0
+	if single {
+		multithreadedThreshold = n + 1
+	}
+
+	w, err := New(
+		tw,
+		Flags(FlagFailOnError),
+		MultithreadedThreshold(multithreadedThreshold),
+	)
+	if err != nil {
+		b.Fatalf("New: %s", err.Error())
+	}
+
+	b.ResetTimer()
+	err = w.Do()
+	b.StopTimer()
+
+	if err != nil {
+		b.Fatalf("Do: %s", err.Error())
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+type testWorker struct {
+	data         []int
+	singleThread int32
+	started      int32
+	finished     int32
+	counter      int32
+	clean        bool
+	withDelay    bool
+}
+
+func (w *testWorker) ElementsCount() int {
+	return len(w.data)
+}
+
+func (w *testWorker) GetElement(idx int) interface{} {
+	return w.data[idx]
+}
+
+func (w *testWorker) ProcInitFunc(workerID int) {
+	if workerID < 0 {
+		atomic.AddInt32(&w.singleThread, 1)
+	}
+	atomic.AddInt32(&w.started, 1)
+}
+
+func (w *testWorker) ProcFinishFunc(workerID int) {
+	atomic.AddInt32(&w.finished, 1)
+}
+
+func (w *testWorker) ProcFunc(idx int, data interface{}) (err error) {
+	if !w.clean {
+		w.data[idx] = -w.data[idx]
+
+		atomic.AddInt32(&w.counter, 1)
+		if w.withDelay {
+			misc.Sleep(100 * time.Microsecond)
+		}
+	}
+	return
 }
 
 //----------------------------------------------------------------------------------------------------------------------------//
--- workers.go
+++ workers.go
@@ -1,6 +1,7 @@
 package workers
 
 import (
+	"fmt"
 	"runtime"
 	"sync"
 
@@ -10,31 +11,85 @@ import (
 
 //----------------------------------------------------------------------------------------------------------------------------//
 
-// Processor --
-type Processor interface {
-	MaxWorkersCount() int
-	ElementsCount() int
-	GetElement(idx int) interface{}
-	ProcInitFunc(workerID int)
-	ProcFinishFunc(workerID int)
-	ProcFunc(idx int, data interface{}) (err error)
-}
+type (
+	// Worker --
+	Worker struct {
+		processor              Processor
+		flags                  Flags
+		maxWorker              int
+		multithreadedThreshold int
+	}
 
-type element struct {
-	idx  int
-	data interface{}
+	// Processor --
+	Processor interface {
+		ElementsCount() int
+		GetElement(idx int) interface{}
+		ProcInitFunc(workerID int)
+		ProcFinishFunc(workerID int)
+		ProcFunc(idx int, data interface{}) (err error)
+	}
+
+	// Flags --
+	Flags uint64
+
+	// MaxWorker --
+	MaxWorker uint
+
+	// MultithreadedThreshold --
+	MultithreadedThreshold uint
+
+	element struct {
+		idx  int
+		data interface{}
+	}
+)
+
+const (
+	// FlagDontUseGetElement --
+	FlagDontUseGetElement = 0x0000001
+	// FlagFailOnError --
+	FlagFailOnError = 0x0000002
+)
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+// New --
+func New(processor Processor, options ...interface{}) (w *Worker, err error) {
+	if processor == nil {
+		err = fmt.Errorf("processor is nil")
+		return
+	}
+
+	w = &Worker{
+		processor: processor,
+	}
+
+	for _, opt := range options {
+		switch opt.(type) {
+		case Flags:
+			w.flags = opt.(Flags)
+		case MaxWorker:
+			w.maxWorker = int(opt.(MaxWorker))
+		case MultithreadedThreshold:
+			w.multithreadedThreshold = int(opt.(MultithreadedThreshold))
+		}
+	}
+
+	return
 }
 
 //----------------------------------------------------------------------------------------------------------------------------//
 
 // Do --
-func Do(p Processor) (err error) {
+func (w *Worker) Do() (err error) {
+	p := w.processor
+
 	elementsCount := p.ElementsCount()
 	if elementsCount <= 0 {
 		return
 	}
 
-	workersCount := p.MaxWorkersCount()
+	workersCount := w.maxWorker
 	if workersCount <= 0 {
 		workersCount = runtime.GOMAXPROCS(-1) * 4
 	}
@@ -43,16 +98,49 @@ func Do(p Processor) (err error) {
 		workersCount = elementsCount
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(int(workersCount))
+	multithreadedThreshold := w.multithreadedThreshold
+	if multithreadedThreshold < 0 {
+		multithreadedThreshold = workersCount * 50
+	}
 
 	msgs := misc.Messages{}
 
+	if workersCount == 1 || multithreadedThreshold > elementsCount {
+		// Singlethread
+		p.ProcInitFunc(-1)
+
+		for i := 0; i < elementsCount; i++ {
+			var data interface{}
+			if w.flags&FlagDontUseGetElement == 0 {
+				data = p.GetElement(i)
+			}
+
+			err := p.ProcFunc(i, data)
+			if err != nil {
+				msgs.AddError(err)
+				if w.flags&FlagFailOnError != 0 {
+					break
+				}
+			}
+		}
+
+		p.ProcFinishFunc(-1)
+
+		err = msgs.Error()
+		return
+	}
+
+	// Multithread
+	var wg sync.WaitGroup
+	wg.Add(int(workersCount))
+
 	queue := make(chan element, elementsCount+1)
-	finished := make(chan bool)
+	finished := make(chan bool, workersCount)
 
 	go func() {
 		defer panic.SaveStackToLog()
+
+		active := true
 
 		for wi := 0; wi < workersCount; wi++ {
 			wi := wi
@@ -67,7 +155,7 @@ func Do(p Processor) (err error) {
 
 				runtime.Gosched()
 
-				for {
+				for active {
 					data, more := <-queue
 
 					// Channel is closed
@@ -84,6 +172,11 @@ func Do(p Processor) (err error) {
 					err := p.ProcFunc(data.idx, data.data)
 					if err != nil {
 						msgs.AddError(err)
+						if w.flags&FlagFailOnError != 0 {
+							active = false
+							finished <- true
+							return
+						}
 					}
 				}
 			}()
@@ -91,9 +184,14 @@ func Do(p Processor) (err error) {
 	}()
 
 	for i := 0; i < elementsCount; i++ {
+		var data interface{}
+		if w.flags&FlagDontUseGetElement == 0 {
+			data = p.GetElement(i)
+		}
+
 		queue <- element{
 			idx:  i,
-			data: p.GetElement(i),
+			data: data,
 		}
 	}
 
@@ -106,11 +204,9 @@ func Do(p Processor) (err error) {
 	close(queue)
 	wg.Wait()
 
-	err = msgs.Error()
-	if err != nil {
-		return
-	}
+	close(finished)
 
+	err = msgs.Error()
 	return
 }
 
